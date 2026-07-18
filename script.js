8a167cfffac692e3aeeb6f8b633b6a20be7fc2fd
Atharva Awasthi <atharva123aa@gmail.com>
2026-07-18 15:30:06 +0530

    hardest if of my life

--- script.js
+++ script.js
@@ -61,7 +61,58 @@ function clamp( v,a,b ){
         this.stemColor=pick(PALETTE.stems);
         this.leafColor= pick(PALETTE.leaves);
         this.flowerCenterColor=pick(PALETTE.flowers);
-        this.flowerCenter= //burnt out a bit will do logic later 5.07
+        this.flowerCenter= 
+  pick(PALETTE.flowerCenter);
+            this.init();
+
+
 
     }
+    init(){
+        const baseY=H-28;
+        const angle=Math.PI/ 2 +rnd(-0.08,0.08);
+        this.root=new Segment(this.x, baseY,angle, rnd(28,38), 5, 
+    this.stemColor,0);
+    this.tips=[this.root];
+    totalStems++;
+
+
+// todo in my final touch make it look more real
+    } 
+grow(ch,speed ){if (!this.tips.length) return;
+    this.LetterFreq[ch]=(this.letterFreq[ch] ||0)+1;
+const isVowel=VOWELS.has(ch.toLowerCase());
+const isSpace=ch ===' ';
+
+const freq=this.LetterFreq[ch]|| 1 
+const tip=
+this.tips[Math.floor(Math.random() * 
+this.tips.length)];
+
+if (isSpace|| (tip.depth>2 && Math.random() <0.4)
+) {this.branch(tip);} else if (isVowel &&  Math.random() <0.7) {
+    if (!tip.flower) {
+        tip.flower={
+            x:tip.ex,
+            y:tip.ey,
+            r: clamp(4 +freq*0.7, 5, 14),color:this.flowerColor,
+            center:this.flowerCenter, 
+            petals:5+ Math.floor(freq/ 2), rot:Math.random()* Math.PI};
+                totalFlowers++;
+        }
+    } else{ this.extend(tip,speed,freq);
+} if (Math.random()> 0.5) this.addLeaf(tip);
+
+}extend(tip,speed,freq) {
+    const wbl=rnd(-0.18, 0.18);
+    const NewAngle=tip.angle+ wbl;
+    const bLen=clamp(20+ speed*0.4 +freq *1.6,14,49);
+    const len= rnd(bLen*0.8,bLen*1.2);
+    const thick=Math.max(1,tip.thick -0.6 );
+    const child=new Segment(tip.ex, tip.ey,NewAngle,len,thick,this.stemColor, tip.depth +1); tip.children.push(child);
+    const idx= this.tips.indexOf(tip);
+    if(idx !== -1) 
+        this.tips.splice(idx,1,child);
+    totalStems++;
+
 }
